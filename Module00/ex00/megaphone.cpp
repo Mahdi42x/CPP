bdb36fd02f83494536d6c6c8efb369b6a608b100
Mahdi42x <146098014+Mahdi42x@users.noreply.github.com>
2025-04-09 18:05:20 +0200

    Add files via upload

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mawada <mawada@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 16:40:11 by mawada            #+#    #+#             */
+/*   Updated: 2025/04/09 17:46:11 by mawada           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype> // für toupper
+
+int main(int argc, char **argv)
+{
+    (void)argv; // Unused variable
+    if(argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+	int i, j;
+	i = 1;
+    while (i < argc)
+    {
+		j = 0;
+		while(argv[i][j] != '\0')
+		{
+			std::cout << (char)toupper(argv[i][j]);
+			j++;
+		}
+		// std::cout << " ";
+		i++;
+    }
+    std::cout << std::endl; // Zeilenumbruch
+    return 0;
+}
